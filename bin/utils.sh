b40b84c144038c5e4650e67dbc58cb8872e99c91
Stratos Staboulis <stratos.staboulis@leanheat.com>
2020-10-19 08:18:30 +0300

    Refactor to bot specific dir structure

---
+++ bin/utils.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+function mkdir_user_data () {
+  local dir=$1
+  local exchange=$2
+  local user_data=$dir/user_data
+  mkdir -p $user_data/backtest_results
+  mkdir -p $user_data/data/$exchange
+  mkdir -p $user_data/logs
+  mkdir -p $user_data/notebooks
+}
+
+
+function freqtrade_wrapper () {
+  # TODO Make a makefile so that this is not hard coded
+  # Activate the freqtrade virtual environment
+  # so that we can run freqtrade
+  source $HOME/src/freqtrade/.env/bin/activate
+  freqtrade $@
+  deactivate
+}
+
